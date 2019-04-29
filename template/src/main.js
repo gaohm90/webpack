69ddd04ff86012a53ad85bb6f4678b96ec049c24
hm <gaohm90@gmail.com>
2019-04-29 22:22:38 +0800

    add more

--- template/src/main.js
+++ template/src/main.js
@@ -8,6 +8,79 @@ import App from './App'
 import router from './router'
 {{/router}}
 
+
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import Icon from 'vue-svg-icon/Icon.vue'
+import {XHeader,Group, Grid, Cell,GridItem, GroupTitle, ViewBox ,XButton,Toast,ToastPlugin,LoadingPlugin} from 'vux'
+
+Vue.use(Vuex)
+Vue.component('icon', Icon)
+Vue.component('x-header', XHeader)
+Vue.component('group', Group)
+Vue.component('cell', Cell)
+Vue.component('grid', Grid)
+Vue.component('grid-item', GridItem)
+Vue.component('group-title', GroupTitle)
+Vue.component('view-box', ViewBox)
+Vue.component('x-button', XButton)
+Vue.component('toast', Toast)
+Vue.use(ToastPlugin)
+Vue.use(LoadingPlugin)
+
+/** i18n vuex设置部分 **/
+import vuexI18n from 'vuex-i18n'
+
+let store = new Vuex.Store({
+  modules: {
+    i18n: vuexI18n.store
+  },
+  state: {    
+    isLoading: false,
+    status:status,    
+    tab_select:0,
+    notice:false,
+    connect:false,
+  },
+  mutations: {
+    changeStatus (state, payload ) {      
+      state.status_now = payload      
+    },
+    changeConnect (state, payload ) {      
+      state.connect = payload      
+    },
+    changeSelect (state, payload ) {      
+      state.tab_select = payload      
+    },
+    changeNotice (state,payload) {
+      state.notice = payload
+    },    
+    updateLoadingStatus (state, payload) {
+      state.isLoading = payload.isLoading
+    },    
+  },
+  actions: {
+    
+  }
+})
+
+Vue.use(vuexI18n.plugin, store)
+
+const translationsEn = {
+	"应用名称": "App Name"
+};
+
+
+Vue.i18n.add('en', translationsEn);
+Vue.i18n.set('en');
+
+store.registerModule('vux', {})
+
+
+
+
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -16,6 +89,7 @@ new Vue({
   {{#router}}
   router,
   {{/router}}
+  store,
   {{#if_eq build "runtime"}}
   render: h => h(App)
   {{/if_eq}}
